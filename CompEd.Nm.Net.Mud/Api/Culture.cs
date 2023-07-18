279de0d8fc69158c91d2cf42b3da451acd82f0bc
Vladimir Zankin <38316266+vzankin@users.noreply.github.com>
2023-07-18 14:31:26 +0400

    - decorations

--- CompEd.Nm.Net.Mud/Api/Culture.cs
+++ CompEd.Nm.Net.Mud/Api/Culture.cs
@@ -4,13 +4,12 @@ namespace CompEd.Nm.Net.Api;
 
 public static class Culture
 {
-    public static void OnGet(HttpResponse response, string culture, string redirectUri)
+    public static void OnGet(HttpResponse response, string culture, string redirect)
     {
-        if (!string.IsNullOrEmpty(culture))
-            response.Cookies.Append(
-                CookieRequestCultureProvider.DefaultCookieName,
-                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture, culture))
+        response.Cookies.Append(
+            CookieRequestCultureProvider.DefaultCookieName,
+            CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture, culture))
         );
-        response.Redirect(redirectUri);
+        response.Redirect(redirect);
     }
 }
