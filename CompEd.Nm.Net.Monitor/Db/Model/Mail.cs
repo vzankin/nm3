314b64db4d4846f53ed2640111df65d42a8041e1
Владимир Занкин <vladimir.zankin@hotmail.com>
2023-09-25 14:31:39 +0400

    removed unnecessary "using"s

--- CompEd.Nm.Net.Monitor/Db/Model/Mail.cs
+++ CompEd.Nm.Net.Monitor/Db/Model/Mail.cs
@@ -1,5 +1,4 @@
-﻿using MailKit;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
